07cec2cc70f73626744b49c89cd93fe73ec2a5d2
SHUBHAM GURME <shubhamgurme52@gmail.com>
2026-07-28 17:01:20 +0530

    Add files via upload

---
+++ assignment1.py
@@ -0,0 +1,21 @@
+#assignment 1 
+
+num1=int(input("Enter The First Number:-"))
+num2=int(input("Enter The Second Number:-"))
+
+print()
+
+print("Addition =",num1+num2)
+print("Substraction=", num1-num2)
+print("Multiplication=", num1*num2)
+print("Division==",num1/num2)
+
+print()
+
+base  = float(input("Enter the Base:"))
+exponent = float(input("Enter the Exponent: "))
+
+print()
+
+result = base ** exponent
+print("Result Of Exponent",result)
